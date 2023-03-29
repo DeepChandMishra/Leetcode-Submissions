27789db428432226f6a1f4c5b7dfe00d0dc176d2
Warsaw <98816042+DeepChandMishra@users.noreply.github.com>
2023-03-29 11:45:37 +0530

    Time: 138 ms (5.01%), Space: 72.3 MB (16.50%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,11 +1,11 @@
 class Solution {
-    int f(int i,int j,vector<vector<int>>& dp,vector<vector<int>> o){
-        if(i>=0 && j>=0 && o[i][j]==1) return 0;
+    int f(int i,int j,vector<vector<int>>& dp,vector<vector<int>> obstacleGrid){
+        if(i>=0 && j>=0 && obstacleGrid[i][j]==1) return 0;
         if(i==0 && j == 0) return 1;
         if(i<0 || j<0 ) return 0;
         if(dp[i][j] != -1) return dp[i][j];
-        int left=f(i-1,j,dp,o);
-        int up=f(i,j-1,dp,o);
+        int left=f(i-1,j,dp,obstacleGrid);
+        int up=f(i,j-1,dp,obstacleGrid);
         return dp[i][j]=left + up;
     }
 public:
