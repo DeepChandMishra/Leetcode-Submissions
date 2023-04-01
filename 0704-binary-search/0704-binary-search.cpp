8c4916a5171b5f6acad3ee3cfd268197f21a9f30
Warsaw <98816042+DeepChandMishra@users.noreply.github.com>
2023-04-01 23:24:46 +0530

    Time: 38 ms (69.66%), Space: 27.6 MB (18.34%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int startIndex = 0; 
+        int lastIndex = nums.size() - 1;
+        while(startIndex <= lastIndex){
+            int midIndex = (startIndex + (lastIndex - startIndex) / 2);
+            if(nums[midIndex] == target){
+                return midIndex;
+            }
+            else if(nums[midIndex] > target){
+                lastIndex = midIndex - 1;
+            }
+            else{
+                startIndex = midIndex + 1;
+            }
+        }
+        return -1;   
+    }
+};
