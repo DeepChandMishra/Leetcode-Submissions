c195b1bf5478f05ba25a3fea31188a123e748f4a
Warsaw <98816042+DeepChandMishra@users.noreply.github.com>
2024-02-15 13:46:56 +0530

    Time: 12 ms (33.07%), Space: 7.1 MB (15.71%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
-    
     int fib(int n) {
-      if(n<=1) return n;
-      int dp[n+1];
-      dp[0]=0;
-      dp[1]=1;
-      for(int i=2;i<n+1;i++){
-          int left=dp[i-1];
-          int right=dp[i-2];
-          dp[i]=left + right;
-      }
-        return dp[n];
+        if(n==0) return 0;
+        if(n==1) return 1;
+        
+        return fib(n-1) + fib(n-2);
     }
 };
